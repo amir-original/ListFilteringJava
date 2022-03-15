82c6ceb88c4093c1ce392989722f0881f979e506
amir-original <amirrahmani7017@gmail.com>
2022-03-15 17:39:07 +0330

    renamed methods in test class.

--- src/test/java/ListFilteringTest.java
+++ src/test/java/ListFilteringTest.java
@@ -8,7 +8,7 @@ public class ListFilteringTest {
 
 
     @Test
-    void should_return_null_if_All_data_In_List_Are_Strings() {
+    void should_Return_Null_If_All_data_In_List_Are_Strings() {
         List list = Arrays.asList("g","gf","a","b");
         ListFilter listFilter = new ListFilter();
         List result = listFilter.filterStrings(list);
@@ -18,7 +18,7 @@ public class ListFilteringTest {
     }
 
     @Test
-    void should_Return_1_2_Between_Strings_And_Numbers_List() {
+    void should_Return_1_2_Between_Strings_And_Integers_List() {
         List list = Arrays.asList(1,2,"a","b");
         ListFilter listFilter = new ListFilter();
 
@@ -29,7 +29,7 @@ public class ListFilteringTest {
     }
 
     @Test
-    void should_return_1_2_0_15_Between_Strings_And_Numbers_List() {
+    void should_return_1_2_0_15_Between_Strings_And_Integers_List() {
         List list = Arrays.asList(1, 2, "a", "b", 0, 15);
         ListFilter listFilter = new ListFilter();
 
@@ -40,7 +40,7 @@ public class ListFilteringTest {
     }
 
     @Test
-    void should_return_1_2_231_Between_Strings_And_Numbers_List() {
+    void should_return_1_2_231_Between_Strings_And_Integers_List() {
         List list = Arrays.asList(1, 2, "a", "b", "aasf", "1", "123", 231);
         ListFilter listFilter = new ListFilter();
 
